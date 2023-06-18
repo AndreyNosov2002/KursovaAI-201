b744d704857a59245ce2f25f3adc7723e921c0c0
AndreyNosov2002 <AndreyNosov2002>
2023-06-18 18:03:56 +0300

    commit message

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "847094c7659248818550b7d050e35c31"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+    }
+}
